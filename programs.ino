461f74c952997c0dd6e438a52dff5973450c2274
Charlee Vonlinsowe <spongebobiscool348@gmail.com>
2026-06-13 22:57:17 -0400

    Create programs.ino
    Ripoff ed frfr

---
+++ programs.ino
@@ -0,0 +1,10 @@
+void Ed(int pid) {
+  Serial.print("File?");
+  while(!Serial.available()) { } // Wait..
+  String fname = Serial.readString();
+  if(!FileExists(fname.c_str())) CreateFile(fname.c_str());
+  Serial.print("$ ");
+  while(!Serial.available()) { } // Wait again..
+  String content = Serial.readString();
+  WriteFile(fname.c_str(), content.c_str(), content.length());
+}
